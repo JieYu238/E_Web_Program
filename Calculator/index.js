2daed4539783e8eff1f50b32354c3d38178aa0b8
Jie(Jee) Yu <jieyu1233@gmail.com>
2024-12-20 16:22:15 -0500

    Create index.js

---
+++ Calculator/index.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function calculate(){
+    try{
+       let expression = display.value;
+       expression = expression.replace(/(\d+)%/g,"($1/100)");
+       const isValid = /^[\d+\-*/().%]*$/.test(expression);
+       if(isValid){
+        display.value = new Function(`return ${expression}`)();
+       }else{
+        display.value = "ERROR";
+       }
+    }
+    catch(error){
+        display.value = "ERROR";
+    }
+}
